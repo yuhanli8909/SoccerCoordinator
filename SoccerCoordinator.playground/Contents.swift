e8dfcf37e13c2019fda80293cf5fd7402fd5ffd1
Yuhan Li <yuhanli@Yuhans-MacBook-Air.local>
2017-01-01 16:23:38 +0800

    project first part

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -5,3 +5,101 @@ import UIKit
 var str = "Hello, playground"
 
 //Yuhan's Comment
+
+var player = [
+    ["Name": "Joe Smith",
+     "Height": 42,
+     "Years of Experience": "YES",
+     "Guardian Name": "Jim and Jan Smith"],
+    ["Name": "Jill Tanner",
+     "Height":36,
+     "Years of Experience": "YES",
+     "Guardian Name":"Clara Tanner"],
+    ["Name": "Bill Bon",
+     "Height":43,
+     "Years of Experience": "YES",
+     "Guardian Name": "Sara and Jenny Bon"],
+    ["Name": "Eva Gordon",
+     "Height":45,
+     "Years of Experience": "NO",
+     "Guardian Name": "Wendy and Mike Gordon"],
+    ["Name": "Matt Gill",
+     "Height":40,
+     "Years of Experience": "NO",
+     "Guardian Name": "Charles and Sylvia Gill"],
+    ["Name": "Kimmy Stein",
+     "Height":41,
+     "Years of Experience": "NO",
+     "Guardian Name": "Bill and Hillary Stein"],
+    ["Name": "Sammy Adams",
+     "Height":45,
+     "Years of Experience": "NO",
+     "Guardian Name": "Jeff Adams"],
+    ["Name": "Karl Saygan",
+     "Height":42,
+     "Years of Experience": "YES",
+     "Guardian Name": "Heather Bledsoe"],
+    ["Name": "Susane Greenberg",
+     "Height": 44,
+     "Years of Experience": "YES",
+     "Guardian Name": "Henrietta Dumas"],
+    ["Name": "Sal Dali",
+     "Height":41,
+     "Years of Experience": "NO",
+     "Guardian Name": "Gala Dali"],
+    ["Name": "Joe Kavalier",
+     "Height":39,
+     "Years of Experience": "NO",
+     "Guardian Name": "Sam and Elaine Kavalier"],
+    ["Name": "Ben Finkelstein",
+     "Height":44,
+     "Years of Experience": "NO",
+     "Guardian Name": "Aeron and Jill Finkelstein"],
+    ["Name": "Diego Soto",
+     "Height":41,
+     "Years of Experience": "YES",
+     "Guardian Name": "Robin and Sarika Soto"],
+    ["Name": "Chloe Alaska",
+     "Height":47,
+     "Years of Experience":"NO",
+     "Guardian Name": "David and Jamie Alaska"],
+    ["Name": "Arnold Willis",
+     "Height":43,
+     "Years of Experience": "NO",
+     "Guardian Name": "Claire Willis"],
+    ["Name": "Philip Helm",
+     "Height":44,
+     "Years of Experience": "YES",
+     "Guardian Name": "Thomas Helm and Eva Jones"],
+    ["Name": "Les Clay",
+     "Height":42,
+     "Years of Experience":"YES",
+     "Guardian Name": "Wynonna Brown"],
+    ["Name": "Herschel Krustofski",
+     "Height":45,
+     "Years of Experience":"YES",
+     "Guardian Name": "Hyman and Rachel Krustofski"]
+]
+
+
+var teamSharks = []
+var teamDragons = []
+var teamRaptors = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
